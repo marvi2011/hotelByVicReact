cb0096ceb84698921fc5471ada46e0432acbd544
Victoria Gramajo <marvi661@hotmail.com>
2024-10-18 17:07:43 -0300

    listo el navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavMenu from './Components/common/NavMenu';
 
 function App() {
-  return <></>;
+  return <>
+  <NavMenu></NavMenu>
+  <h1 className='text-center mt-3'>HOTEL REFUGIO DE LAS NUBES</h1>
+  </>;
 }
 
 export default App;
