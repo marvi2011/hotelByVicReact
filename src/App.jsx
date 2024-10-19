c22cc6ad89026c572e98f2a3096de9f56369b43f
Victoria Gramajo <marvi661@hotmail.com>
2024-10-18 22:38:29 -0300

    listo el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavMenu from './Components/common/NavMenu';
+import Footer from './Components/common/Footer';
 
 function App() {
   return <>
   <NavMenu></NavMenu>
   <h1 className='text-center'>HOTEL REFUGIO DE LAS NUBES</h1>
+  <Footer></Footer>
   </>;
 }
 
