9e737df949e9ae7f3eeb02187b1ccb63c73be4f1
Victoria Gramajo <marvi661@hotmail.com>
2024-10-21 19:04:16 -0300

    comenz+e la estructura delcomponente

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavMenu from "./Components/common/NavMenu";
 import Index from "./pages/Index";
 import Footer from "./Components/common/Footer";
+import Administrador from "./pages/Administrador";
 
 function App() {
   return (
     <section>
-      <NavMenu></NavMenu>
-    <Index></Index>
+     <NavMenu></NavMenu>
+  <Administrador></Administrador>
       <Footer></Footer>
     </section>
   );
---
+++ src/pages/Administrador.jsx
@@ -0,0 +1,32 @@
+import { Table } from "react-bootstrap";
+
+const Administrador = () => {
+  return (
+    <div>
+      <section className="container mainSection">
+        <div className="d-flex justify-content-between align-items-center mt-5 container-fluid bg-success-subtle text-center py-5">
+          <h1 className="display-4">Habitaciones disponibles</h1>
+        
+        </div>
+        <hr />
+        <Table responsive striped bordered hover>
+          <thead>
+            <tr className="text-center">
+              <th>Cod</th>
+              <th>Número de Habitación</th>
+              <th>Precio</th>
+              <th>URL de Imagen</th>
+              <th>Categoria</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+          </tbody>
+        </Table>
+      </section>
+    </div>
+  );
+};
+
+export default Administrador;
