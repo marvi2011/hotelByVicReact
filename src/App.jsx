835916e7a7824c7cde8319a44672a6fed495efcb
Victoria Gramajo <marvi661@hotmail.com>
2024-10-22 22:02:34 -0300

    arreglando el maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import NavMenu from "./Components/common/NavMenu";
 import Index from "./pages/Index";
 import Footer from "./Components/common/Footer";
 import Administrador from "./pages/Administrador";
+import Habitaciones from "./pages/habitaciones/Habitaciones";
 
 function App() {
   return (
     <section>
      <NavMenu></NavMenu>
-  <Administrador></Administrador>
+  {/*<Administrador></Administrador>*/}
+<Index></Index>
       <Footer></Footer>
     </section>
   );
